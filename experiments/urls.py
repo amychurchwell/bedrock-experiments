7fcceda26be00a9befe5114975690d9f1286b91f
Amy Churchwell <achurchwell@mozilla.com>
2019-04-19 14:03:27 -0700

    Add question views and admin

--- experiments/urls.py
+++ experiments/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('dashboard/', include('dashboard.urls')),
+    path('intake/', include('intake.urls')),
     path('admin/', admin.site.urls),
 ]
